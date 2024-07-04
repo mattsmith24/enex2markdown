73247ff3daaca437c6f6d11306fc56df0bacc51c
Matthew Smith <yo.checkit@gmail.com>
2024-07-04 14:43:09 +1000

    Add CLI arguments support
    Got rid of hard-coded paths and logging level

--- README.md
+++ README.md
@@ -13,6 +13,21 @@ If you want to run this:
 cd enex2markdown
 pipenv install
 pipenv run pytest tests
-vim enex2markdown.py # edit the file paths because I hard-coded them
-pipenv run python enex2markdown.py
+pipenv run python enex2markdown.py INPUT_FILENAME
+```
+
+Help output:
+
+```
+usage: ENEX2Markdown [-h] [-o OUTPUT_DIR] [-l {debug,info,warning,error,critical}] input_filename
+
+Converts an EXEX export file from Evernote to a directory of markdown files and attachments.
+
+positional arguments:
+  input_filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT_DIR, --output-dir OUTPUT_DIR
+  -l {debug,info,warning,error,critical}, --log-level {debug,info,warning,error,critical}
 ```
--- src/enex2markdown.py
+++ src/enex2markdown.py
@@ -1,14 +1,40 @@
 
 import logging
+import argparse
 
 from enex_parser import EnexParser
 from note import NoteWriter
 
 logger = logging.getLogger("enex2markdown")
 logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
+
+def get_cli_args():
+    argparser = argparse.ArgumentParser(
+        prog='ENEX2Markdown',
+        description='Converts an EXEX export file from Evernote to a directory of markdown files and attachments.',
+        )
+    argparser.add_argument('input_filename')
+    argparser.add_argument('-o', '--output-dir', default='.')
+    argparser.add_argument('-l', '--log-level', default="warning", choices=["debug", "info", "warning", "error", "critical"])
+    return argparser.parse_args()
+
+def set_logging_level(level_str):
+    level_dict = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL
+    }
+    logger.setLevel(level_dict[level_str])
+
+def main():
+    args = get_cli_args()
+    set_logging_level(args.log_level)
+    parser = EnexParser()
+    parser.register_note_listener(NoteWriter(args.output_dir))
+    parser.parseNoteXML(args.input_filename)
 
 if __name__ == '__main__':
-    parser = EnexParser()
-    parser.register_note_listener(NoteWriter('f:\\yocheckit\\Documents\\Temp'))
-    parser.parseNoteXML('f:\\yocheckit\\dropbox\\2024\\matts24s-notebook.enex')
+    main()
